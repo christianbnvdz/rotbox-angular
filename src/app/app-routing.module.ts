8ef1e7f3064bc0798339f4f7d3f6eea9d55db0a8
Christian Benavidez <christianbenavidez57@gmail.com>
2022-04-26 21:27:34 -0700

    feat: set up routing to generated view components
    Add routing outlet to app component html and add paths to the generated
    components in the app routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FilesComponent} from './files/files.component';
+import { UploadComponent} from './upload/upload.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'files', component: FilesComponent },
+  { path: 'upload', component: UploadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
