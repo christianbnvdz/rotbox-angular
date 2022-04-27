b9e7aa1ff57e49be1c2aabe24d94463ea424ccba
Christian Benavidez <christianbenavidez57@gmail.com>
2022-04-26 21:17:35 -0700

    chore: generate components to use in routing
    Generate four components, each corresponding to one of the four views
    in the app component html router links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FilesComponent } from './files/files.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FilesComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
