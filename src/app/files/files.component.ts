892d0a9d4e56f871a4978f2682847413765c24ae
Christian Benavidez <christianbenavidez57@gmail.com>
2022-04-28 18:34:37 -0700

    feat: get file list from api to display in view
    Grab the file list by making an api call using the file service and hook up
    the response with the variables defined in the template. Dont set up the
    download button api call yet.

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FileService } from '../file.service';
+
 @Component({
   selector: 'app-files',
   templateUrl: './files.component.html',
   styleUrls: ['./files.component.css']
 })
 export class FilesComponent implements OnInit {
+  files: any;
 
-  constructor() { }
+  constructor(private fileService: FileService) { }
 
   ngOnInit(): void {
+    this.fileService.getFiles().subscribe(
+      (resp) => {
+        this.files = resp.files;
+      }
+    );
   }
 
+  downloadFile(): void {
+
+  }
 }
